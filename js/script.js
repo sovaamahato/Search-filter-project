000cbf456f483102e558a7c0ecd704602d22cf94
sova1916 <mahatosova618@gmail.com>
2020-09-22 09:05:52 +0545

    search/filter functionality

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const searchfun = () =>{
+    let filter = document.getElementById('myInput').value.toUpperCase();
+    let myTable = document.getElementById('myTable');
+    let tr = myTable.getElementsByTagName('tr');
+
+    for (var i=0; i<tr.length; i++){
+        let td = tr[i].getElementsByTagName('td')[0];
+        if (td){
+            let textvalue = td.textContent || td.innerHTML;
+            if(textvalue.toUpperCase().indexOf(filter) > -1){
+                tr[1].style.display="";
+            }else{
+                tr[i].style.display="none";
+            }
+        }
+    }
+}
